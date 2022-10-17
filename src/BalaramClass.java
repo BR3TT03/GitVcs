e7a2997b0ab83040f97561a185407cb9a4135043
BR3TT03 <rockbrett03@gmail.com>
2022-10-17 19:28:14 +0545

    balaram ko changes haru

--- src/BalaramClass.java
+++ src/BalaramClass.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class BalaramClass {
+public class BalaramClass {
+    public static void main(String[] args) {
+        System.out.println("balaram ko changes haru");
+    }
 }
